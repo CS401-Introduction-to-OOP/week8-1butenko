91eb4a03c375cb15f69f64d0b820b3f1c11ed92c
1butenko <ihor.butenko@icloud.com>
2026-03-28 14:10:40 +0200

    Letter class with custom calculate cost function

---
+++ Week8/Letter.cs
@@ -0,0 +1,9 @@
+public class Letter : DeliveryItem
+{
+    public Letter(string trackingNumber, double weight) : base(trackingNumber, weight){}
+
+    public override double CalculateCost()
+    {
+        return 15 + (Weight * 10);
+    }
+}
