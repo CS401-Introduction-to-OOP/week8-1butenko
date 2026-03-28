06c004f0204ed68753bb97d512d50aed8d154c81
1butenko <ihor.butenko@icloud.com>
2026-03-28 14:03:50 +0200

    Renamed DeliveryNumber -> DeliveryItem

---
+++ Week8/DeliveryItem.cs
@@ -0,0 +1,15 @@
+public abstract class DeliveryItem
+{
+    public string TrackingNumber { get; }
+    public double Weight { get; protected set; }
+
+    public DeliveryItem(string trackingNumber, double weight)
+    {
+        TrackingNumber = trackingNumber;
+        Weight = weight;
+    }
+
+    public abstract double CalculateCost();
+
+    public abstract void PrintInfo();
+}
