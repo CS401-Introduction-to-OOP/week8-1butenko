40f0b23e73ed51b5124e35872c777bf9cb2ee475
1butenko <ihor.butenko@icloud.com>
2026-03-28 14:32:06 +0200

    Test for class instances

--- Week8/Program.cs
+++ Week8/Program.cs
@@ -1,3 +1,18 @@
-﻿// See https://aka.ms/new-console-template for more information
+﻿DeliveryItem l1 = new Letter("xyz123", 125);
+DeliveryItem l2 = new Letter("qwerty123", 10);
+DeliveryItem p1 = new Parcell("zxc1456", 12, "30x30x30");
+DeliveryItem p2 = new Parcell("fgh345", 12, "10x20x100");
 
-Console.WriteLine("Hello, World!");
+l1.PrintInfo();
+l2.PrintInfo();
+p1.PrintInfo();
+p2.PrintInfo();
+
+CargoContainer<DeliveryItem> myCargo = new CargoContainer<DeliveryItem>();
+
+myCargo.AddItem(l1);
+myCargo.AddItem(l2);
+myCargo.AddItem(p1);
+myCargo.AddItem(p2);
+
+Console.WriteLine($"Total cost: {myCargo.GetTotalCost()}");
